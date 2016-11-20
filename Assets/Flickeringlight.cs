940d1d08b020cf288beecd9e5032a899d5c89dfe
hkturtco <imhktc@gmail.com>
2016-11-20 21:00:10 +0800

    Background Noise, Flickering Light , ground texture

---
+++ Assets/Flickeringlight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flickeringlight : MonoBehaviour {
+	Light lightsource;
+
+	// Use this for initialization
+	void Start () {
+		lightsource = GetComponent<Light>();
+		StartCoroutine(Flashing());
+	}
+
+	IEnumerator Flashing()
+	{
+		while(true){
+			yield return new WaitForSeconds(0.1f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.1f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.2f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.5f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.1f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.1f);
+			lightsource.enabled =! lightsource.enabled;
+			yield return new WaitForSeconds(0.1f);
+			lightsource.enabled =! lightsource.enabled;
+		}
+	}
+}
