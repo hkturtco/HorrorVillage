9eea0f888524b20b32dc7ba88bfdb6f258a8b78d
hkturtco <imhktc@gmail.com>
2016-11-22 04:16:04 +0800

    Door and Key

---
+++ Assets/door.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+	public static bool key;
+	public bool open;
+	public bool close;
+	public bool trigger;
+
+	void OnTriggerEnter(Collider other){
+		trigger = true;
+	}
+	void OnTriggerExit (Collider other){
+		trigger = false;
+	}
+
+	void Update()
+	{		
+		if(trigger){
+			if(close){
+				if(key){
+					if(Input.GetKeyDown(KeyCode.E)){
+						open = true;
+						close = false;
+					}
+				}
+			} else {
+					if(Input.GetKeyDown(KeyCode.E)){
+						close = true;
+						open = false;
+					}
+			}
+		}
+
+		if(trigger){
+			if(open){
+				var ro = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 90.0f, 0.0f), Time.deltaTime*200);
+				transform.rotation = ro;
+			} else {
+				var ro = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime*200);
+				transform.rotation = ro;
+			}
+		}
+	}
+	void OnGUI(){
+		if(trigger){
+			if(open){
+				GUI.Box(new Rect(0,0,200,25), "O");
+			} else {
+				if(key){
+					GUI.Box(new Rect(0,0,200,25), "O");
+					} else {
+					GUI.Box(new Rect(0,0,200,25), "Need key");
+					}
+				
+			}
+		}
+	}
+}
---
+++ Assets/key.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class key : MonoBehaviour {
+	public bool trigger;
+
+	void OnTriggerEnter(Collider other){
+		trigger = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		trigger = false;
+	}
+
+	void Update(){
+		if(trigger){
+			if(Input.GetKeyDown(KeyCode.E)){
+				door.key = true;
+				Destroy(this.gameObject);
+			}
+		}
+	}
+
+}
