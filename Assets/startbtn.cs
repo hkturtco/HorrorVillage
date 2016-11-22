114bd379367ad17a2b8e7856e90ec500d52fc248
hkturtco <imhktc@gmail.com>
2016-11-22 16:10:39 +0800

    Start button

---
+++ Assets/startbtn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ using UnityEngine.UI;
+
+public class startbtn : MonoBehaviour {
+
+      public void test(int index)
+      {
+          Application.LoadLevel(index);
+      }
+  
+      public void test2(string levelName)
+      {
+          Application.LoadLevel(levelName);
+      }
+}
