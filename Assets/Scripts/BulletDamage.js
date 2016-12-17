e6dfa3b0c8a793371e0c5eb5a4d8cfcdc22dad30
grapekit <grapekit@hotmail.com>
2016-12-17 15:38:39 +0800

    enemy health bar, enemy destory, gun sound

---
+++ Assets/Scripts/BulletDamage.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var bulletDamage = 20;
+
+function OnCollisionEnter (info : Collision){
+	
+	info.transform.SendMessage("ApplyDamage",bulletDamage,SendMessageOptions.DontRequireReceiver);
+	Destroy(gameObject);
+}
---
+++ Assets/Scripts/EnemyHealth.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var maxHealth =1000;
+var Health = 1000;
+var scale=1f;
+var enemyHP : GameObject;
+enemyHP = GameObject.Find("Health");
+function ApplyDamage (TheDamage:int) {
+ Health -= TheDamage;
+ scale=parseFloat((Health*1.0f)/maxHealth);
+ enemyHP.gameObject.transform.localScale = new Vector3(scale, 1f, 1f);
+   
+ if(Health <=0){
+	 Dead();
+ }
+}
+
+function Dead () {
+	Destroy(gameObject);
+}
