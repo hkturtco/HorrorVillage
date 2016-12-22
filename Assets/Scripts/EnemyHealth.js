4b70672a729ca43d2d20f9bc8c809f65c2630e68
Dingyuan Shen <shendy6011@gmail.com>
2016-12-22 20:42:12 +0800

    monster health bug fix

--- Assets/Scripts/EnemyHealth.js
+++ Assets/Scripts/EnemyHealth.js
@@ -6,15 +6,16 @@ var enemyHP : GameObject;
 var monster_animator : Animator;
 var audio_gethit : GameObject;
 function ApplyDamage (TheDamage:int) {
- 	Health -= TheDamage;
- 	scale=parseFloat((Health*1.0f)/maxHealth);
- 	enemyHP.gameObject.transform.localScale = new Vector3(scale, 1f, 1f);
- 	var animatorstateinfo = monster_animator.GetCurrentAnimatorStateInfo(0);
- 	if (!animatorstateinfo.IsName("GetHit")){
-			monster_animator.SetTrigger("GetHit");
-			audio_gethit.gameObject.SetActive(true);
-	}
-
+	if (Health >0){
+	 	Health -= TheDamage;
+	 	scale=parseFloat((Health*1.0f)/maxHealth);
+	 	enemyHP.gameObject.transform.localScale = new Vector3(scale, 1f, 1f);
+	 	var animatorstateinfo = monster_animator.GetCurrentAnimatorStateInfo(0);
+	 	if (!animatorstateinfo.IsName("GetHit")){
+				monster_animator.SetTrigger("GetHit");
+				audio_gethit.gameObject.SetActive(true);
+		}
+	}	
  	if(Health <=0){
 	 	monster_animator.SetTrigger("dead");
  	}
